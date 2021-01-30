c2abfd9aecff6f9e7f24d1b85e5594d18e71e252
Damoon Imani <damoonimani@pm.me>
2021-01-31 00:41:40 +0330

    New minor version

--- lib/tellus.rb
+++ lib/tellus.rb
@@ -1,6 +1,10 @@
 require "tellus/version"
 
-
+## Tellus : Mother Earth
+## to read this gem:
+## 1. look at client.rb
+## 2. look how cli works using thor
+## 3. check the tests to understand the features
 module Tellus
   class Error < StandardError; end
 end
--- lib/tellus/client.rb
+++ lib/tellus/client.rb
@@ -1,5 +1,8 @@
 require "httparty"
 
+# Client is a new nomnatim.openstreetmap.org client instance
+# it changes url to match latitude and longitude
+# default zoom is 14 to fetch just enough info about the address
 module Tellus
   class Client
     attr_reader :latitude, :longitude, :url
